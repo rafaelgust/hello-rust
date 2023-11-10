33d6259bd798b47846bc860670d8da7f24b05358
rafaelgust <rafael.appdevs@gmail.com>
2023-11-10 20:45:15 -0300

    Add largest function and tests to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use warp::Filter;
+/* use warp::Filter;
 #[tokio::main]
 async fn main() {
    simple_api().await;
@@ -10,4 +10,38 @@ async fn simple_api() {
     .map(|name| format!("Hello, {}!", name));
 
     warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+} */
+
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 101];
+    let result = largest(&number_list);
+
+    print!("The largest number is {}", result);
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_largest() {
+        let number_list = vec![34, 50, 25, 100, 65];
+        assert_eq!(largest(&number_list), 100);
+
+        let char_list = vec!['y', 'm', 'a', 'q'];
+        assert_eq!(largest(&char_list), 'y');
+    }
 }
